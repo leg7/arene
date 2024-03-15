3121d2d3b14cbd23f5539f7fa1998aa8fb19e2b8
rubenzhu <rubenzhu@hotmail.com>
2024-03-15 10:54:36 +0100

    t

--- main.cpp
+++ main.cpp
@@ -12,12 +12,11 @@ int main()
     //initialise la graine du générateur aléatoire
     std::srand(std::time(nullptr));
     //
-    // // création de l'Arbitre (graine , joueur 1, joueur 2 , nombre de parties)
-    // Arbitre a (9999, player::RAND, player::BRUTAL_,100);
-    // // commence le challenge
-    // a.challenge();
+    // création de l'Arbitre (graine , joueur 1, joueur 2 , nombre de parties)
+    Arbitre a (9999, player::RAND, player::BRUTAL_,100);
+    // commence le challenge
+    a.challenge();
 
-    Jeu j(9999);
 
 
 
